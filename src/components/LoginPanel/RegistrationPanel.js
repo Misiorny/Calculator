52df662739b1b1631b140b5bd200b35dc474446d
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-08-03 21:51:59 +0200

    feat: add another new components

--- src/components/LoginPanel/RegistrationPanel.js
+++ src/components/LoginPanel/RegistrationPanel.js
@@ -1,18 +1,17 @@
-import styles from '../../styles/styles.module.css';
-import NameForm from '../Form/NameForm';
-import SurnameForm from '../Form/SurnameForm';
-import EmailForm from '../Form/EmailForm';
+import styles       from '../../styles/styles.module.css';
+import NameForm     from '../Form/NameForm';
+import SurnameForm  from '../Form/SurnameForm';
+import EmailForm    from '../Form/EmailForm';
 import PasswordForm from '../Form/PasswordForm';
+import StreetForm   from '../Form/StreetForm';
+import CodeForm     from '../Form/CodeForm';
 
-function RegistrationPanel(){
-  return(
+function RegistrationPanel() {
+  return (
 	  <form action="" className={styles.login}>
 		<div>
-		  <NameForm/>
-		  <SurnameForm/>
-		  <EmailForm/>
-		  <PasswordForm label='Enter'/>
-		  <PasswordForm label='Confirm'/>
+		  <NameForm/> <SurnameForm/> <EmailForm/> <PasswordForm label='Enter'/> <PasswordForm label='Confirm'/>
+		  <StreetForm/> <CodeForm/>
 		</div>
 	  </form>
   )
